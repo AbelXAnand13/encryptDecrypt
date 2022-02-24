059f0e65b453290413c2135b035f6562d68ca4eb
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-23 20:00:34 -0800

    final commit2, this it i think

--- encryptDecrypt/main.cpp
+++ encryptDecrypt/main.cpp
@@ -3,26 +3,47 @@
 using namespace std;
 int main() {
     ifstream in;
-    ofstream out("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/encryptedFile.txt");
-    in.open("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/decryptedFile.txt");
+    ofstream out;
     
     //to encrypt var in is the decrypted file and to decrypt var in is the encrypted file
     string txt;
+    
+    in.open("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/edtxt.txt");
+    out.open("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/encryptedFile.txt");
     if (!in.is_open()) {
             cout << "File not found\n";
         }
+    
     bool quit = false;
     int key;
+    //key
     while (!quit) {
-    cout << "Enter the encryption or decryption key\n";
-    cin >> key;
-        if (key > 94 || key < -94) {
-            continue;
+        cout << "Enter the key (you can do negatives or positives, just remember to toggle negative sign)\n";
+        cin >> key;
+        if (key > 94) {
+            key = 94;
+        }
+        else if (key < -94) {
+            key = -94;
         }
         else {
             quit = true;
         }
     }
+    
+    
+    while (getline(in, txt)) {
+        for (int i = 0; i < txt.length(); i++) {
+            unsigned char ch = txt[i] + key;
+            out << ch;
+        }
+        out << endl;
+    }
+    in.close();
+    out.close();
+    in.open("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/encryptedFile.txt");
+    out.open("/Users/Abel/Desktop/Cpp/encryptDecrypt/encryptDecrypt/decryptedFile.txt");
+    
     while (getline(in, txt)) {
         for (int i = 0; i < txt.length(); i++) {
             unsigned char ch = txt[i] - key;
@@ -32,4 +53,4 @@ int main() {
     }
     in.close();
     out.close();
-}
+    }
